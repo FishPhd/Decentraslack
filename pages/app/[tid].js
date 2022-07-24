a670c80f9230d7a74a89d72c84165a03a185278f
Sam Fish <samuel.fish@live.com>
2022-07-23 18:21:59 -0700

    presudo topics

--- pages/app/[tid].js
+++ pages/app/[tid].js
@@ -5,25 +5,7 @@ import { useRouter } from 'next/router'
 
 const topic_id = '0.0.47732850'
 
-const navigation = [
-	{
-		name: `Personal`,
-		href: `/app/${topic_id}`,
-		icon: UsersIcon,
-		current: true
-	}
-	// {
-	// 	name: `Private`,
-	// 	href: '/app',
-	// 	icon: UsersIcon,
-	// 	current: false
-	// }
-]
-
 function useStickyState(defaultValue, key) {
-	const router = useRouter()
-	const { tid } = router.query
-
 	if (typeof window !== 'undefined') {
 		const [value, setValue] = useState(() => {
 			const stickyValue = window.localStorage.getItem(key)
@@ -34,7 +16,7 @@ function useStickyState(defaultValue, key) {
 		}, [key, value])
 		return [value, setValue]
 	}
-	return [undefined, undefined]
+	return [undefined, () => {}]
 }
 
 function classNames(...classes) {
@@ -42,8 +24,18 @@ function classNames(...classes) {
 }
 
 export default function Layout({ props }) {
+	const router = useRouter()
+	const { tid } = router.query
 	const [sidebarOpen, setSidebarOpen] = useState(false)
 	const [messages, setMessages] = useStickyState([], 'messages')
+	const [topics, setTopics] = useState([
+		{
+			name: `Personal`,
+			href: `/app/${topic_id}`,
+			icon: UsersIcon,
+			current: true
+		}
+	])
 	const [curMessage, setCurMessage] = useState(String)
 
 	const fetchData = async ({ message, reference }) => {
@@ -56,7 +48,7 @@ export default function Layout({ props }) {
 			body: JSON.stringify({
 				message,
 				reference,
-				topic_id,
+				tid,
 				allow_synchronous_consensus: true
 			})
 		})
@@ -131,9 +123,9 @@ export default function Layout({ props }) {
 									</div>
 									<div className="mt-5 flex-1 h-0 overflow-y-auto">
 										<nav className="px-2 space-y-1">
-											{navigation.map(item => (
+											{topics?.map((item, index) => (
 												<a
-													key={item.name}
+													key={item.name + index}
 													href={item.href}
 													className={classNames(
 														item.current
@@ -169,7 +161,7 @@ export default function Layout({ props }) {
 						</div>
 						<div className="mt-5 flex-1 flex flex-col">
 							<nav className="flex-1 px-2 pb-4 space-y-1">
-								{navigation.map(item => (
+								{topics?.map(item => (
 									<a
 										key={item.name}
 										href={item.href}
@@ -187,6 +179,25 @@ export default function Layout({ props }) {
 										{item.name}
 									</a>
 								))}
+								<button
+									onClick={() => {
+										const id = '0.0.47732851'
+										setTopics(arr => [
+											...arr,
+											{
+												name: `Private`,
+												id,
+												href: `/app/${id}`,
+												icon: UsersIcon,
+												current: tid === id
+											}
+										])
+									}}
+									type="button"
+									className="inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 xl:w-full"
+								>
+									New Channel
+								</button>
 							</nav>
 						</div>
 					</div>
@@ -196,7 +207,7 @@ export default function Layout({ props }) {
 						<div className="py-6">
 							<div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
 								<h1 className="text-2xl font-semibold text-gray-900">
-									Topic {topic_id}
+									Topic {tid}
 								</h1>
 							</div>
 							<div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
